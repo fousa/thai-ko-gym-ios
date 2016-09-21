bbd512b217346bbf0f4512e1d6f1a7f1e4c71041
Jelle Vandebeeck <jelle@fousa.be>
2016-09-21 07:55:32 +0200

    Add a simple authentication model.

---
+++ Thai Ko Gym/Components/Networking/Models/Authentication.swift
@@ -0,0 +1,15 @@
+//
+//  Authentication.swift
+//  Thai Ko Gym
+//
+//  Created by Jelle Vandebeeck on 21/09/2016.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+import Foundation
+
+struct Authentication {
+
+    
+
+}
